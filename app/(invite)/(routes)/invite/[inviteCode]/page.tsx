7a27cdb410f0fa4523898ce1ac511eb341afc1da
WebRush-Kwork <webrushgithub@gmail.com>
2025-04-15 01:27:52 +0500

    refactor: comment out InviteCodePage implementation for future review

--- app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -1,33 +1,33 @@
-import { currentProfile } from '@/lib/current-profile'
-import { db } from '@/lib/db'
-import { redirect } from 'next/navigation'
+// import { currentProfile } from '@/lib/current-profile'
+// import { db } from '@/lib/db'
+// import { redirect } from 'next/navigation'
 
-interface IInviteCodePage {
-	params: { inviteCode: string }
-}
+// interface IInviteCodePage {
+// 	params: { inviteCode: string }
+// }
 
-const InviteCodePage = async ({ params }: IInviteCodePage) => {
-	const profile = await currentProfile()
-	const { inviteCode } = params
+// const InviteCodePage = async ({ params }: IInviteCodePage) => {
+// 	const profile = await currentProfile()
+// 	const { inviteCode } = params
 
-	if (!inviteCode) throw new Error('Invite code not found')
+// 	if (!inviteCode) throw new Error('Invite code not found')
 
-	if (!profile || !inviteCode) return redirect('/')
+// 	if (!profile || !inviteCode) return redirect('/')
 
-	const existingServer = await db.server.findFirst({
-		where: { inviteCode, members: { some: { profileId: profile.id } } },
-	})
+// 	const existingServer = await db.server.findFirst({
+// 		where: { inviteCode, members: { some: { profileId: profile.id } } },
+// 	})
 
-	if (existingServer) return redirect(`/servers/${existingServer.id}`)
+// 	if (existingServer) return redirect(`/servers/${existingServer.id}`)
 
-	const server = await db.server.update({
-		where: { inviteCode },
-		data: { members: { create: [{ profileId: profile.id }] } },
-	})
+// 	const server = await db.server.update({
+// 		where: { inviteCode },
+// 		data: { members: { create: [{ profileId: profile.id }] } },
+// 	})
 
-	if (server) return redirect(`/servers/${server.id}`)
+// 	if (server) return redirect(`/servers/${server.id}`)
 
-	return null
-}
+// 	return null
+// }
 
-export default InviteCodePage
+// export default InviteCodePage
